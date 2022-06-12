053c8d6a4855088a64c916f45652e5a8dd0daba4
mjseok <mjseok98@gmail.com>
2022-06-12 23:45:30 +0900

    [FE] 필요한 컴포넌트 추가

---
+++ frontend/src/components/common/Button.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Button=()=>{
+    return(
+        <ButtonWrapper>
+
+        </ButtonWrapper>
+    )
+
+}
+const ButtonWrapper=styled.button`
+    
+`;
+export default Button;
